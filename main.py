fd1029c8b2a7811cd639fce517d4112a1d991a42
Luis Alberto Gonzáles Usca (Luchin) <64605259+Luchin151090@users.noreply.github.com>
2022-06-15 14:56:16 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,31 +1,48 @@
+import kivy
+kivy.require('1.9.0')
+
 from kivy.app import App
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
-from kivy.uix.widget import Widget
+from kivy.uix.label import Label
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.button import Button
+from kivy.uix.boxlayout import BoxLayout
 import requests
 
-####### APLICACION DEL CLIMA METODO GET #############
-class Contenedor(Widget):
-   
-    def consultar(self):
+class Interfaz(GridLayout):
+    def __init__(self, **kwargs):
+        super(Interfaz,self).__init__(**kwargs)
+        self.cols=3
 
-        #recuperamos el dato del text_input
-        ciudad = self.ids.entrada.text
+        self.ciudad=TextInput(hint_text='Enter city',multiline=False)
+        self.add_widget(self.ciudad)
+
+        self.submit = Button(text='Submit')
+        self.submit.bind(on_press=self.consultar)
+        self.add_widget(self.submit)
+
+        self.resultado=Label(text='Esperando consulta')
+        self.add_widget(self.resultado)
+    
+    def consultar(self,obj):
+        ciudad = str(self.ciudad.text)
         url="https://api.openweathermap.org/data/2.5/weather?q={}&appid=14361693170d23bbffec093892b8f1c7"
-        nueva_url = url.format(ciudad)
+        nueva_url=url.format(ciudad)
         respuesta=requests.get(nueva_url)
         data = respuesta.json()
         temp =data["main"]["temp"]
         min = data["main"]["temp_min"]
         max = data["main"]["temp_max"]
         pais = data["sys"]["country"]
-
-        self.lbl.text="Temp normal C°: "+str(temp-273.15)+"\n"+"Mínima:"+str(min-273.15)+"\n"+"Máxima:"+str(max-273.15)+"\n"+"País:"+str(pais)
+        self.resultado.text="Temp normal C°: "+str(temp-273.15)+"\n"+"Mínima:"+str(min-273.15)+"\n"+"Máxima:"+str(max-273.15)+"\n"+"País:"+str(pais)
         
 
-class MainApp(App):
-    def build(self):
-        return Contenedor()
 
-MainApp().run()
+class ClimaApp(App):
+    def build(self):
+        return Interfaz()
+
+
+
+if __name__=="__main__":
+    ClimaApp().run()
